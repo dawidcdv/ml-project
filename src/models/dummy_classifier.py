5d4ce7ca717b853da708a2ec07e58a6216e6fe06
dawidcdv <djozwiak@edu.cdv.pl>
2022-06-09 20:18:30 +0200

    Fix dummy clasiffier - load processed train data

--- src/models/dummy_classifier.py
+++ src/models/dummy_classifier.py
@@ -4,7 +4,7 @@ from sklearn.dummy import DummyClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from src.features.helpers import absolute_path
-from src.data.make_dataset import load_raw_train_data, load_labels
+from src.data.make_dataset import load_labels, load_train_data
 
 
 def get_dummy_classifier(X_train, y_train, cache=True):
@@ -25,7 +25,7 @@ def score_dummy(X_train, y_train, X_test, y_test):
 
 
 def main():
-    X = load_raw_train_data()
+    X = load_train_data()
     y = load_labels()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=2022)
     score_dummy(X_train, y_train, X_test, y_test)
