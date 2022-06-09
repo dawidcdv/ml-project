0f6a7d639562f8b312dbf2611235d480f7e307c9
dawidcdv <djozwiak@edu.cdv.pl>
2022-06-09 21:15:22 +0200

    Add missing load train data function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -22,6 +22,10 @@ def load_raw_train_data():
     return pd.read_csv(absolute_path('data', 'raw', 'train_data.csv'),  header=None)
 
 
+def load_train_data():
+    return pd.read_csv(absolute_path('data', 'processed', 'train_data.csv'),  header=None)
+
+
 def load_labels():
     return pd.read_csv(absolute_path('data', 'raw', 'train_labels.csv'),  header=None)
 
