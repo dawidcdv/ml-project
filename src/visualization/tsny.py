e4431f6f61c889d24c43095ea7a78e2511a07c55
MarekM0 <mmilosierny@edu.cdv.pl>
2022-06-23 11:31:49 +0200

    Make scatter plots for tsne and pca_tsna

--- src/visualization/tsny.py
+++ src/visualization/tsny.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import os
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
@@ -9,13 +10,36 @@ from src.data.make_dataset import load_raw_train_data, load_labels
 from src.features.helpers import absolute_path
 
 
-X = load_raw_train_data()
-y = load_labels()
+def main():
+    X = load_raw_train_data()
+    y = load_labels()
 
-scaler = StandardScaler()
-X_std = scaler.fit_transform(X)
+    scaler = StandardScaler()
+    X_std = scaler.fit_transform(X)
 
-tsne = TSNE()
-X_tsne = tsne.fit_transform(X_std)
-print(X_tsne.shape)
+    tsne = TSNE()
+    X_tsne = tsne.fit_transform(X_std)
+    print(X_tsne.shape)
 
+    sns.set(rc={'figure.figsize':(10,10)})
+    palette = ['tab:red', 'tab:green']
+    sns.scatterplot(data = X_tsne, x = X_tsne[:,0], y=X_tsne[:,1], hue=y[0], palette=palette)
+    plt.savefig(absolute_path("reports","figures","tsne_scatter.png"))
+
+    pca = PCA(n_components=0.99)
+    X_pca = pca.fit_transform(X_std)
+    print('PCA n_components: ', pca.n_components_)
+
+    pca = PCA(n_components=3558)
+    X_pca = pca.fit_transform(X)
+
+    tsne = TSNE()
+    X_pca_tsne = tsne.fit_transform(X_pca)
+
+    sns.scatterplot(data = X_pca_tsne, x = X_pca_tsne[:,0], y=X_pca_tsne[:,1], hue=y[0], palette=palette)
+
+    plt.savefig(absolute_path("reports","figures","tsne_pca_scatter.png"))
+
+
+if __name__ == '__main__':
+    main()
