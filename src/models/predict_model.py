0dbb16128e00510556bf6d49a0292d5fec2dbbd5
dawidcdv <djozwiak@edu.cdv.pl>
2022-06-26 13:21:33 +0200

    Add predict model and result

---
+++ src/models/predict_model.py
@@ -0,0 +1,25 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from src.data.make_dataset import load_raw_train_data, load_labels, load_test_data
+from src.features.helpers import absolute_path
+from src.models.svc import create_svc_classifier
+
+
+def main():
+    X = load_raw_train_data()
+    y = load_labels()
+    test_data = load_test_data()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=42)
+    sc = StandardScaler(with_std=False)
+    X_train = sc.fit_transform(X_train,y_train)
+
+    classifier= create_svc_classifier()
+    classifier.fit(X_train, y_train)
+    predict_labels = classifier.predict(test_data)
+    np.savetxt(absolute_path('data','processed','predictions.csv'), predict_labels, fmt='%i')
+
+
+if __name__ == '__main__':
+    main()
